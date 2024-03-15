919bbc4dcd7a3ad268c78875e0f7ea893d53c6ca
HenryHolt1 <154479368+HenryHolt1@users.noreply.github.com>
2024-03-15 17:30:11 -0400

    Added Images to Buttons on NAv Bar
    Added Images to Buttons on NAv Bar

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,45 +1,34 @@
 import React from 'react';
 import './App.css';
 
+import homered from './homered.png'; 
+import tasklist from './bulletred.png';
+import calendar from './calendarred.png';
+import settings from './gear.png';
+import help from './questionmark.jpg';
+import account from'./personicon.webp';
+
 function App() {
   return (
     <div className="App">
-      <body>
-        <div id="page home">
+      <div id="navbar" className="navbar">
+        <nav>
+          <a href="#"><img src={homered} height='40' width='40' alt="Home" /></a>
+          <a href="#"><img src={tasklist} height='40' width='40' alt="Task List" /></a>
+          <a href="#"><img src={calendar} height='40' width='40' alt="Calendar" /></a>
+        </nav>
 
-          <div id="navbar" className="navbar">
-            <nav className="buttons">
-              <a href="#">Home</a>
-              <a href="#">Task List</a>
-              <a href="#">Calendar</a>
-            </nav>
-
-            <div className="searchBar">
-              <input type="text" placeholder="Search..." />
-              <button type="submit">Search</button>
-            </div>
-
-            <nav className="buttons">
-              <a href="#">Settings</a>
-              <a href="#">Help</a>
-              <a href="#">Account</a>
-            </nav>
-          </div>
-
-          <div id="app" className="app">
-            <div className="newTask">
-              
-            </div>
-            <div mid>
-
-            </div>
-            <div bottom>
-
-            </div>
-          </div>
-
+        <div className="searchBar">
+          <input type="text" placeholder="Search..." />
+          <button type="submit">Search</button>
         </div>
-      </body>
+
+        <nav className="buttons">
+          <a href="#"><img src={settings} height='40' width='40' alt="Settings" /></a>
+          <a href="#"><img src={help} height='40' width='40' alt="Help" /></a>
+          <a href="#"><img src={account} height='40' width='40' alt="Account" /></a>
+        </nav>
+      </div>
     </div>
   );
 }
