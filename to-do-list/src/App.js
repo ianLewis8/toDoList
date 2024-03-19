d0328c05ea60b2ab6cf6f9f5e73bb2bedfdf020a
whhlct <39563523+whhlct@users.noreply.github.com>
2024-03-19 15:27:31 -0400

    made buttons styling consistent

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <div id="navbar" className="navbar">
-        <nav>
+        <nav className="buttons">
           <a href="#"><img src={homered} height='40' width='40' alt="Home" /></a>
           <a href="#"><img src={tasklist} height='40' width='40' alt="Task List" /></a>
           <a href="#"><img src={calendar} height='40' width='40' alt="Calendar" /></a>
