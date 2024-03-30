6f5e5067d288ce43191de245503a1bbc18135453
ianLewis8 <ianlewis319@gmail.com>
2024-03-29 23:50:05 -0400

    New Website Design

--- to-do-list/src/home.js
+++ to-do-list/src/home.js
@@ -1,9 +1,11 @@
+import './home.css';
+
 function Home() {
   return (
     <div className="Home">
-      <h1>
-        Home page
-      </h1>
+      <div className="newTask">
+          
+      </div>
     </div>
   );
 }
