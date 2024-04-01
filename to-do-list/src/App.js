ab7b770f5c9d148e3f793c3783841a26d2692620
ianLewis8 <ianlewis319@gmail.com>
2024-04-01 19:45:08 -0400

    Start of add task bar

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
 import './App.css';
-import homered from './homered.png';
-import tasklist from './bulletred.png';
-import calendar from './calendarred.png';
-import settings from './gear.png';
-import help from './questionmark.jpg';
-import account from './personicon.webp';
 
 function App() {
   return (
     <div className="App">
       <div id="navbar" className="navbar">
-        <nav className="buttons">
-          <Link to="/"><img src={homered} height='40' width='40' alt="Home" /></Link>
-          <Link to="/tasklist"><img src={tasklist} height='40' width='40' alt="Task List" /></Link>
-          <Link to="/calendar"><img src={calendar} height='40' width='40' alt="Calendar" /></Link>
+        <nav className="buttons"> 
+          <Link to="/"><div className='symbol'>home</div></Link>
+          <Link to="/tasklist"><div className='symbol'>format_list_bulleted</div></Link>
+          <Link to="/calendar"><div className='symbol'>calendar_month</div></Link>
         </nav>
 
         <div className="searchBar">
@@ -24,9 +18,9 @@ function App() {
         </div>
 
         <nav className="buttons">
-          <Link to="/settings"><img src={settings} height='40' width='40' alt="Settings" /></Link>
-          <Link to="/help"><img src={help} height='40' width='40' alt="Help" /></Link>
-          <Link to="/account"><img src={account} height='40' width='40' alt="Account" /></Link>
+          <Link to="/settings"><div className='symbol'>settings</div></Link>
+          <Link to="/help"><div className='symbol'>help</div></Link>
+          <Link to="/account"><div className='symbol'>account_circle</div></Link>
         </nav>
       </div>
       <Outlet />
